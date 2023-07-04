dc0dbecdb904ec553d72ecd65692e7525f3ceb68
GabriellyDiasPereira <gabriellydiaspereira15@gmail.com>
2023-07-04 11:37:20 -0300

    valor

--- js/main.js
+++ js/main.js
@@ -9,6 +9,8 @@ var peso = tdPeso.textContent
 var tdAltura = paciente.querySelector(".info-altura")
 var altura = tdAltura.textContent
 
+var tdImc = paciente.querySelector(".info-imc")
+
 var imc = peso / (altura * altura) // 100 / 2.0 * 2.0 = 100 / 4 => 25
 
-console.log(imc)
+tdImc.textContent = imc
