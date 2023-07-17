cf64f085848f2f2f17ae11cbf9047a5625903793
GabriellyDiasPereira <gabriellydiaspereira15@gmail.com>
2023-07-17 14:59:45 -0300

    adicionando pacientes na tabela

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ let titulo = document.querySelector(".titulo");
 titulo.textContent = "Maçã";
 
 var pacientes = document.querySelectorAll(".paciente")
-console.log("Lista completa:" , pacientes)
 
 for(var i = 0; i < pacientes.length; i++){
 
@@ -43,5 +42,33 @@ if( alturaEValida && pesoEValido){
 var botaoAdicionar = document.querySelector("#adicionar-paciente")
 botaoAdicionar.addEventListener('click', function(event){
     event.preventDefault()
-    console.log('Ei')
+    
+    var form = document.querySelector('#form-adiciona')
+    
+    var nome = form.nome.value
+    var peso = form.peso.value
+    var altura = form.altura.value
+    var gordura = form.gordura.value
+
+    var pacienteTr = document.createElement('tr')
+
+    var nomeTd = document.createElement('td')
+    var pesoTd = document.createElement('td')
+    var alturaTd = document.createElement('td')
+    var gorduraTd = document.createElement('td')
+    var imcTd = document.createElement('td')
+
+    nomeTd.textContent = nome
+    pesoTd.textContent = peso
+    alturaTd.textContent = altura
+    gorduraTd.textContent = gordura
+
+    pacienteTr.appendChild(nomeTd)
+    pacienteTr.appendChild(pesoTd)
+    pacienteTr.appendChild(alturaTd)
+    pacienteTr.appendChild(gorduraTd)
+    
+    var tabela = document.querySelector('#tabela-pacientes')
+
+    tabela.appendChild(pacienteTr)
 })
