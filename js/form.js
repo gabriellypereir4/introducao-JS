c6af8fe0e02faa9f4da33aaff187956e6d87076e
GabriellyDiasPereira <gabriellydiaspereira15@gmail.com>
2023-07-31 12:00:56 -0300

    função reset

--- js/form.js
+++ js/form.js
@@ -14,6 +14,8 @@ botaoAdicionar.addEventListener('click', function(event){
     var tabela = document.querySelector('#tabela-pacientes')
 
     tabela.appendChild(pacienteTr)
+
+    form.reset()
 })
 
 
@@ -34,23 +36,11 @@ function montaTr(paciente){
     var pacienteTr = document.createElement('tr')
     pacienteTr.classList.add('paciente')
 
-    var nomeTd = montaTd(paciente.nome, 'info-nome')
-    var pesoTd = montaTd(paciente.peso, 'info-peso')
-    var alturaTd = montaTd(paciente.altura, 'info-altura')
-    var gorduraTd = montaTd(paciente.gordura, 'info-gordura')
-    var imcTd = montaTd(paciente.imc, 'info-imc')
-
-    nomeTd.textContent = paciente.nome
-    pesoTd.textContent = paciente.peso
-    alturaTd.textContent = paciente.altura
-    gorduraTd.textContent = paciente.gordura
-    imcTd.textContent = paciente.imc
-
-    pacienteTr.appendChild(nomeTd)
-    pacienteTr.appendChild(pesoTd)
-    pacienteTr.appendChild(alturaTd)
-    pacienteTr.appendChild(gorduraTd)
-    pacienteTr.appendChild(imcTd)
+    pacienteTr.appendChild(montaTd(paciente.nome, 'info-nome'))
+    pacienteTr.appendChild(montaTd(paciente.peso, 'info-peso'))
+    pacienteTr.appendChild(montaTd(paciente.altura, 'info-altura'))
+    pacienteTr.appendChild(montaTd(paciente.gordura, 'info-gordura'))
+    pacienteTr.appendChild(montaTd(paciente.imc, 'info-imc'))
 
     return pacienteTr
 }
